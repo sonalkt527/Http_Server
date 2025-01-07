e7e34e89331906fd61fa51c8bca86a9bee012876
sonal1 <sonalk527@gmail.com>
2025-01-08 00:24:07 +0530

    Static page creation

--- src/main/java/com/sonal/httpserver/HttpServer.java
+++ src/main/java/com/sonal/httpserver/HttpServer.java
@@ -5,6 +5,8 @@ import com.sonal.httpserver.config.Configuration;
 import com.sonal.httpserver.config.ConfigurationManager;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -22,11 +24,41 @@ public class HttpServer {
         Configuration conf= ConfigurationManager.getInstance().getCurrentConfiguration();
 
         System.out.println("using port: "+ conf.getPort());
-        System.out.println("using port: "+ conf.getWebroot());
+        System.out.println("using webroot: "+ conf.getWebroot());
 
         try {
             ServerSocket serverSocket= new ServerSocket(conf.getPort());
            Socket socket= serverSocket.accept();
+
+           InputStream inputStream =socket.getInputStream();
+           OutputStream outputStream=socket.getOutputStream();
+
+           //TODO we wourld read
+
+            String html="<html><head><title>simple java server</title></head><body>Simple Java http server</body>";
+            //after status line two special characters are sent
+           final  String CRLF="\n\r";
+            String response= "HTTP/1.1 200 OK"+CRLF+
+                    "Content Length: "+html.getBytes().length+CRLF+ //header
+                    CRLF+
+                    html+
+                    CRLF+
+                    CRLF; //status line : HTTP_VERSION RESPONSE_CODE RESPONSE_MESSAGE
+
+
+
+
+
+            //TODO we would write
+
+            outputStream.write(response.getBytes());
+
+            inputStream.close();
+            outputStream.close();
+            serverSocket.close();
+            socket.close();
+
+
 
 
 
